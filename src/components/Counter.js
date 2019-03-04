029a5a8dce35f0844e28bf565d7a692331bc871e
Frank Martinez <frankie.martinez@gmail.com>
2019-03-04 15:21:37 -0800

    MVP reached, further studying and stretches begins

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -18,23 +18,25 @@ class Counter extends Component {
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
         return (
-            <p>
-                Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
-                    +
-                </button>
-                <button onClick={() => {/* Fill me in */ }}>
-                    -
-                </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
-            </p>
+            <section className='container'>
+                <p>
+                    Clicked: {this.props.count} times
+                    <button onClick={() => this.props.increment()}>
+                        +
+                    </button>
+                    <button onClick={() => this.props.decrement()}>
+                        -
+                    </button>
+                    {/* Uncomment these button tags if you got
+                    around to implementing the extra credit functions */}
+                    {/* <button onClick={this.incrementIfOdd}>
+                        Increment if odd
+                    </button>
+                    <button onClick={this.incrementAsync}>
+                        Increment async
+                    </button>  */}
+                </p>
+            </section>
         );
     }
 }
@@ -47,7 +49,8 @@ class Counter extends Component {
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
     return {
-        count: state.count
+        // count: state.count // Why?
+        count: state // Why?
     };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,19 +3,25 @@ import { INCREMENT, DECREMENT } from '../actions';
 const initialState = {
   count: 0
 }
-
+console.log(initialState.count)
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
-export default (state = initialState, action) => {
+export default (state = initialState.count, action) => {
+  console.log('In the reducer yo', action);
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return ++state;
     case DECREMENT:
-    // Fill in the body of this case
+      // return --count;
+      return --state;
+      // return {
+      //   ...state
+      // }
     default:
       return state;
+      // return count;
   }
 };
